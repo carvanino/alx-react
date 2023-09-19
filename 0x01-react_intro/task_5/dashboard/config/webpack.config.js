5890fadd539f6e8ec244f3b3b7caa301b1320d86
Akinola <akinolatofunmi04@gmail>
2023-09-19 17:03:13 +0100

    9. Create a project using Webpack

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(?:ico|git|png|jpg|jpeg)$/i,
+                type: 'asset/resource'
+            },
+
+        ]
+    }
+}
